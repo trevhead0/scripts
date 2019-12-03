83524cf2c7ae2278884edbaf5e240ee118ff9dc4
Trevor <38106995+trevhead0@users.noreply.github.com>
2019-12-03 01:40:34 -0600

    asddas

---
+++ info.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
+ <?php
+$servername = serverip;
+$username = dbuname;
+$password = upass;
+$dbname = dbnamee;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, Title, PosterName, Content FROM comment";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["ID"]. " - Title: " . $row["PosterName"]. " " . $row["Content"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?> 
+</body>
+</html>
